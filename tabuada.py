b5b516b995c34c6da3b2d3b0bc7f93cea0c27798
isaacsaless <isaacnascimentosales@gmail.com>
2023-10-11 11:14:00 -0300

    Primeira versão do código de mostrar a tabuada

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+# Digite um numero interio e veja sua tabuada
+print('Descubra a tabuada de um número qualquer')
+numero = int(input('Digite um número inteiro: '))
+tabuada1 = numero*1
+tabuada2 = numero*2
+tabuada3 = numero*3
+tabuada4 = numero*4
+tabuada5 = numero*5
+tabuada6 = numero*6
+tabuada7 = numero*7
+tabuada8 = numero*8
+tabuada9 = numero*9
+tabuada10 = numero*10
+print(f'A tabuada do seu número é {tabuada1}, {tabuada2}, {tabuada3}, {tabuada4}, {tabuada5}, {tabuada6}, {tabuada7}, {tabuada8}, {tabuada9}, {tabuada10}.')
